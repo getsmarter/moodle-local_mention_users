0c9a3095a6c7f8320256a40a607a5d88428185a0
kurvinh <khendricks@2u.com>
2021-02-16 19:27:33 +0200

    CTED-1708 remove observer as it has moved to mod hsu_forum

--- db/events.php
+++ db/events.php
@@ -27,9 +27,5 @@ $observers = array (
     array (
         'eventname' => '\mod_forum\event\assessable_uploaded',
         'callback'  => 'local_mention_users_observer::email_mention',
-    ),
-    array (
-        'eventname' => '\mod_hsuforum\event\assessable_uploaded',
-        'callback'  => 'local_mention_users_observer::email_mention_hsu',
     )
 );
