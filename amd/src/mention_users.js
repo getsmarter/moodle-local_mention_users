31e7bfe97961ba35808c0c3e8ed6b98e5233ed4e
Brendon Pretorius <bpretorius@2u.com>
2021-12-20 10:38:18 +0200

    Only clear on submit

--- amd/src/mention_users.js
+++ amd/src/mention_users.js
@@ -104,6 +104,13 @@ define(['jquery', 'core/ajax', 'local_mention_users/tribute'], function($, ajax)
         useridpassed = true;
         windowhashash = window.location.hash !== '';
         user = users_array.filter(function(item) {return item.value == userid[1]})[0];
+        window.addEventListener('hashchange', function() {
+          setTimeout(function() {
+            if (userid)
+            $('.hsuforum-textarea').empty();
+            $('#hiddenadvancededitoreditable').empty();
+          }, 1000);
+        });
       }
 
       // Atto Editor
@@ -163,13 +170,6 @@ define(['jquery', 'core/ajax', 'local_mention_users/tribute'], function($, ajax)
     var shiftWindow = function() { scrollBy(0, -70) };
     if (location.hash) shiftWindow();
     window.addEventListener("hashchange", shiftWindow);
-    window.addEventListener('hashchange', function() {
-      setTimeout(function() {
-        $('.hsuforum-textarea').empty();
-        $('#hiddenadvancededitoreditable').empty();
-      }, 1000);
-    });
-
     getUsers(reply_id, forum_id, advanced_forum);
   };
   return module;
